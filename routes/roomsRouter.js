e510112fb646a35e559e81c0eff3e43ce91077b5
tugacomum <internetsoundseasy@gmail.com>
2023-09-11 09:21:22 +0100

    teste

--- routes/roomsRouter.js
+++ routes/roomsRouter.js
@@ -13,6 +13,15 @@ router.post("/getallrooms", async (req, res) => {
   }
 });
 
+router.get("/getallrooms", async (req, res) => {
+  try {
+    const rooms = await Room.find({});
+    res.send(rooms);
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+});
+
 router.post("/getroombyid", async (req, res) => {
   const roomid = req.body.roomid;
   try {
